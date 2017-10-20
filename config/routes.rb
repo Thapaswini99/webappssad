4660aacee0b69e230a4c8505b64c65b887fed662
Thapaswini99 <vibhana1962@gmail.com>
2017-10-20 15:01:26 +0530

    changes in twitter login

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
